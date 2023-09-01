01f599eb0dfc562c47c3b31674e17e9c01734745
Andrey <andreysgd@gmail.com>
2023-09-01 08:27:09 -0300

    programming concepts on rust

---
+++ programming_concepts/target/debug/deps/programming_concepts-7776ab15b02d578a.d
@@ -0,0 +1,5 @@
+/home/andreysgd/projects/rust/programming_concepts/target/debug/deps/programming_concepts-7776ab15b02d578a.rmeta: src/main.rs
+
+/home/andreysgd/projects/rust/programming_concepts/target/debug/deps/programming_concepts-7776ab15b02d578a.d: src/main.rs
+
+src/main.rs:
---
+++ programming_concepts/target/debug/deps/programming_concepts-938cfb517ed7a5c8.d
@@ -0,0 +1,5 @@
+/home/andreysgd/projects/rust/programming_concepts/target/debug/deps/programming_concepts-938cfb517ed7a5c8: src/main.rs
+
+/home/andreysgd/projects/rust/programming_concepts/target/debug/deps/programming_concepts-938cfb517ed7a5c8.d: src/main.rs
+
+src/main.rs:
