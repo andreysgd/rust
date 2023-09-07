ad365ab9454bc02a8f239c308e4315bfa3e6e6f5
Andrey <andreysgd@gmail.com>
2023-09-07 06:29:15 -0300

    examples of programming concepts in rust

---
+++ factorial_non_negative_integer/target/debug/deps/factorial_non_negative_integer-0ec823e72a772f1f.d
@@ -0,0 +1,5 @@
+/home/andreysgd/projects/rust/factorial_non_negative_integer/target/debug/deps/factorial_non_negative_integer-0ec823e72a772f1f: src/main.rs
+
+/home/andreysgd/projects/rust/factorial_non_negative_integer/target/debug/deps/factorial_non_negative_integer-0ec823e72a772f1f.d: src/main.rs
+
+src/main.rs:
