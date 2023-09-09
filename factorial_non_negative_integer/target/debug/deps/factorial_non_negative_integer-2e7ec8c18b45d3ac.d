070536676b99719582947b3082daf5cbac2762b7
Andrey <andreysgd@gmail.com>
2023-09-08 21:19:56 -0300

    factorial of a non-negative integer

---
+++ factorial_non_negative_integer/target/debug/deps/factorial_non_negative_integer-2e7ec8c18b45d3ac.d
@@ -0,0 +1,5 @@
+/home/andreysgd/projects/rust/factorial_non_negative_integer/target/debug/deps/factorial_non_negative_integer-2e7ec8c18b45d3ac.rmeta: src/main.rs
+
+/home/andreysgd/projects/rust/factorial_non_negative_integer/target/debug/deps/factorial_non_negative_integer-2e7ec8c18b45d3ac.d: src/main.rs
+
+src/main.rs:
---
+++ factorial_non_negative_integer/target/debug/deps/factorial_non_negative_integer-88da12620501421b.d
@@ -0,0 +1,5 @@
+/home/andreysgd/projects/rust/factorial_non_negative_integer/target/debug/deps/factorial_non_negative_integer-88da12620501421b.rmeta: src/main.rs
+
+/home/andreysgd/projects/rust/factorial_non_negative_integer/target/debug/deps/factorial_non_negative_integer-88da12620501421b.d: src/main.rs
+
+src/main.rs:
