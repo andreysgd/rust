b1aa3ee2035af16d2f41c574f2067f3c18655f8a
Andrey <andreysgd@gmail.com>
2023-09-10 23:07:45 -0300

    greatest common divisor program

---
+++ greatest_common_divisor/target/debug/deps/greatest_common_divisor-8ce6245e55117b63.d
@@ -0,0 +1,5 @@
+/home/andreysgd/projects/rust/greatest_common_divisor/target/debug/deps/greatest_common_divisor-8ce6245e55117b63.rmeta: src/main.rs
+
+/home/andreysgd/projects/rust/greatest_common_divisor/target/debug/deps/greatest_common_divisor-8ce6245e55117b63.d: src/main.rs
+
+src/main.rs:
