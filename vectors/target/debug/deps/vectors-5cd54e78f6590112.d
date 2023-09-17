ea807ad6418a3ae77886b6c5015a5cb791dbc665
Andrey <andreysgd@gmail.com>
2023-09-17 14:54:35 -0300

    opening, reading and writing files

---
+++ vectors/target/debug/deps/vectors-5cd54e78f6590112.d
@@ -0,0 +1,5 @@
+/home/andreysgd/projects/rust/vectors/target/debug/deps/vectors-5cd54e78f6590112: src/main.rs
+
+/home/andreysgd/projects/rust/vectors/target/debug/deps/vectors-5cd54e78f6590112.d: src/main.rs
+
+src/main.rs:
---
+++ vectors/target/debug/vectors.d
@@ -0,0 +1,1 @@
+/home/andreysgd/projects/rust/vectors/target/debug/vectors: /home/andreysgd/projects/rust/vectors/src/main.rs
